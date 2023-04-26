14ead25bba41e60ac63c3367fcede0d739acc199
Anton Kalén <anton.kalen@gmail.com>
2023-04-26 09:51:47 +0200

    Added reading the raw data to the pipeline.

--- _targets.R
+++ _targets.R
@@ -9,8 +9,8 @@ library(targets)
 
 # Set target options:
 tar_option_set(
-  packages = c("tibble"), # packages that your targets need to run
-  format = "rds" # default storage format
+  packages = c("here"), # packages that your targets need to run
+  format = "parquet" # default storage format
   # Set other options as needed.
 )
 
@@ -26,13 +26,5 @@ tar_source()
 
 # Replace the target list below with your own:
 list(
-  tar_target(
-    name = data,
-    command = tibble(x = rnorm(100), y = rnorm(100))
-#   format = "feather" # efficient storage of large data frames # nolint
-  ),
-  tar_target(
-    name = model,
-    command = coefficients(lm(y ~ x, data = data))
-  )
+  tar_target(raw_data, here("data/swgt-data.csv.gz"), format = "file")
 )
