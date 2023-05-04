b044c6587a3d55f8a3462afd3f4b53fd26eb47a6
Anton Kalén <anton.kalen@gmail.com>
2023-05-04 16:42:56 +0200

    Cleaned up comments in _targets.R

--- _targets.R
+++ _targets.R
@@ -1,46 +1,42 @@
-# Created by use_targets().
-# Follow the comments below to fill in this target script.
-# Then follow the manual to check and run the pipeline:
-#   https://books.ropensci.org/targets/walkthrough.html#inspect-the-pipeline # nolint
 
-# Load packages required to define the pipeline:
+# Load packages to run pipeline -------------------------------------------
+
 library(targets)
-# library(tarchetypes) # Load other packages as needed. # nolint
 
-# Set target options:
+
+# Set options -------------------------------------------------------------
+
 tar_option_set(
-  packages = c("here", "arrow", "dplyr", "rsample") # packages that your targets need to run
+  packages = c("here", "arrow", "dplyr", "rsample"),
   # format = "parquet" # default storage format
-  # Set other options as needed.
+  seed = 746
 )
 
 
-# tar_make_clustermq() configuration (okay to leave alone):
-options(clustermq.scheduler = "multicore")
+# Source functions in R/ --------------------------------------------------
 
-# tar_make_future() configuration (okay to leave alone):
-# Install packages {{future}}, {{future.callr}}, and {{future.batchtools}} to allow use_targets() to configure tar_make_future() options.
-
-# Run the R scripts in the R/ folder:
 tar_source()
 
-# Replace the target list below with your own:
+
+
+# List of targets ---------------------------------------------------------
+
 list(
-  # Reads the raw strokes data into the pipeline
+  ## Reads the raw strokes data into the pipeline -------------------------
   tar_target(strokes_data_file, here("data/swgt-data.csv.gz"), format = "file"),
   tar_target(raw_strokes_data, read_csv_arrow(strokes_data_file)),
 
-  # Clean data
+  ## Clean data -----------------------------------------------------------
   tar_target(cleaned_strokes_data, clean_strokes_data(raw_strokes_data)),
 
-  # Create classification of competition levels and merge back with clean data
+  ## Classify competition levels and merge back with clean data -----------
   tar_target(comp_levels, create_comp_levels(cleaned_strokes_data)),
   tar_target(
     strokes_data_comp_level,
     left_join(comp_levels, cleaned_strokes_data, by = join_by(user_pk, season))
   ),
 
-  # Split training/validation
+  # Split training/validation ---------------------------------------------
   tar_target(strokes_data_binned, bin_strokes_data(strokes_data_comp_level)),
   tar_target(
     train_test_split,
